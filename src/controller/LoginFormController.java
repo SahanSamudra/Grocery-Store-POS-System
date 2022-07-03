7abdadd62bf2a513c1cfeeed7529874fdcbd0339
SahanSamudra <sahansamudra123@gmail.com>
2022-07-03 14:07:48 +0530

    first commit

---
+++ src/controller/LoginFormController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginFormController {
+    public AnchorPane LoginFormContext;
+    public JFXTextField UserName;
+    public JFXPasswordField Password;
+    public Label lblError;
+
+    public void btnAdminLogin(ActionEvent actionEvent) {
+    }
+
+    public void btnCashierLogin(ActionEvent actionEvent) {
+    }
+}
